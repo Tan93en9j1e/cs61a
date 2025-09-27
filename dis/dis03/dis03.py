83eab224f5bd8f48bae4cf48ad903ae68f7b25d4
Tan93en9j1e <3124894818@qq.com>
2025-09-27 17:54:09 +0800

    dis03完成了

--- dis/dis03/dis03.py
+++ dis/dis03/dis03.py
@@ -90,3 +90,42 @@ def odd(n):
     else:
         return hailstone(n*3+1)+1
 
+
+def sevens(n, k):
+    """Return the (clockwise) position of who says n among k players.
+
+    >>> sevens(2, 5)
+    2
+    >>> sevens(6, 5)
+    1
+    >>> sevens(7, 5)
+    2
+    >>> sevens(8, 5)
+    1
+    >>> sevens(9, 5)
+    5
+    >>> sevens(18, 5)
+    2
+    """
+    def f(i, who, direction):
+        if i == n:
+            return who
+        "*** YOUR CODE HERE ***"
+        if i%7==0 or has_seven(i):
+            direction=-direction
+        next_who=who+direction
+        if next_who > k:
+            next_who =1
+        elif next_who <1:
+            next_who=k
+        return f(i+1,next_who,direction)
+    return f(1, 1, 1)
+
+def has_seven(n):
+    if n == 0:
+        return False
+    elif n % 10 == 7:
+        return True
+    else:
+        return has_seven(n // 10)
+
